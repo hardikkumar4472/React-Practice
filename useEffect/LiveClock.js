0bec8763077c048e7f1438eb18677252272a473f
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-10-30 19:43:45 +0530

    Add LiveClock component to display current time

---
+++ useEffect/LiveClock.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return <h2>{time}</h2>;
+}
